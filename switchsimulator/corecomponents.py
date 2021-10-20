d1a7f4002e694f5000143a1848c32135fa84f8f7
LoePhi <76783060+LoePhi@users.noreply.github.com>
2021-10-20 15:55:50 +0200

    first steps

---
+++ switchsimulator/corecomponents.py
@@ -0,0 +1,143 @@
+from component import ElectricComponent
+
+
+class CoreComponent(ElectricComponent):
+    """
+    Parent Class for all core components
+    This are the basic buidling blocks that all
+    other components are assembled from
+
+    All core components have a singlaur output-line
+    called 'out_a'
+    """
+
+    outputs = ElectricComponent.unpack_io('out_a', )
+
+    def get_state(self):
+        return(self.out_a)
+
+    is_on = property(get_state)
+
+
+class Switch(CoreComponent):
+    """
+    Simple Switch
+    It carries a current when it is closed
+    """
+
+    def __init__(self, closed: bool = False):
+        self.closed = closed
+        self.setup()
+
+    def build_circuit(self):
+        pass
+
+    def compute_state(self):
+        self.out_a = self.closed
+
+    def flip(self):
+        self.out_a = not self.out_a
+        self.forward_pass()
+
+    def open(self):
+        self.out_a = False
+        self.forward_pass()
+
+    def close(self):
+        self.out_a = True
+        self.forward_pass()
+
+# TODO: hier comupte und so überarbeiten
+class Connector(CoreComponent):
+    """
+    The connector is used for addressing indiviudal output lines
+    of components that feauture multiple outputs.
+    """
+
+    inputs = ElectricComponent.unpack_io('in_a')
+
+    def __init__(self, in_a: ElectricComponent, port: str):
+        self.in_a = in_a
+        self.port = port
+        self.setup()
+
+    def build_circuit(self):
+        self.in_a.add_connection(self, '_in_a')
+
+    def compute_state(self):
+        self.out_a = self.in_a.get_state(self.port)
+
+
+class LooseWire(CoreComponent):
+    """
+    This component is used solely for initializing unconnected inputs
+    It never carries a current
+    """
+
+    # HACK: This makes LooseWire() cretae new instances
+    # when used as default function paramter. Might not be needed
+    # when/if all arguments are parsed
+    __slots__ = ['out_a']
+
+    def __init__(self):
+        self.setup()
+
+    def build_circuit(self):
+        pass
+
+    def compute_state(self):
+        self.out_a = False
+
+
+class INV(CoreComponent):
+    """Inverts the input"""
+
+    inputs = ElectricComponent.unpack_io('in_a', )
+
+    def __init__(self, in_a: ElectricComponent = LooseWire()):
+        self.in_a = in_a
+        self.setup()
+
+    def build_circuit(self):
+        self.in_a.add_connection(self, 'in_a')
+
+    def compute_state(self):
+        self.out_a = not self.in_a.get_state()
+
+
+class AND(CoreComponent):
+    """AND-Gate"""
+
+    inputs = ElectricComponent.unpack_io('in_a', 'in_b')
+
+    def __init__(self, in_a: ElectricComponent = LooseWire(),
+                 in_b: ElectricComponent = LooseWire()):
+        self.in_a = in_a
+        self.in_b = in_b
+        self.setup()
+
+    def build_circuit(self):
+        self.in_a.add_connection(self, 'in_a')
+        self.in_b.add_connection(self, 'in_b')
+
+    def compute_state(self):
+        self.out_a = self.in_a.get_state() and self.in_b.get_state()
+
+
+class OR(CoreComponent):
+    """OR-Gate"""
+
+    inputs = ElectricComponent.unpack_io('in_a', 'in_b')
+
+    def __init__(self, in_a: ElectricComponent = LooseWire(),
+                 in_b: ElectricComponent = LooseWire()):
+        self.in_a = in_a
+        self.in_b = in_b
+        self.setup()
+
+    def build_circuit(self):
+        self.in_a.add_connection(self, 'in_a')
+        self.in_b.add_connection(self, 'in_b')
+
+    def compute_state(self):
+        self.out_a = self.in_a.get_state() or self.in_b.get_state()
--- switchsimulator/logicgates.py
+++ switchsimulator/logicgates.py
@@ -1,81 +1,49 @@
 from component import ElectricComponent
-from singlestatecomp import SingleStateComponent, LooseWire
+from corecomponents import LooseWire, INV, OR, AND, Switch
 
 
-class INV(SingleStateComponent):
-    """Inverts the input"""
-
-    inputs = ElectricComponent.unpack_io('in_a', )
-
-    def __init__(self, in_a: ElectricComponent = LooseWire()):
-        self.in_a = in_a
-        self.setup()
-
-    def build_circuit(self):
-        self.in_a.add_connection(self, 'in_a')
+class IntegratedComponent(ElectricComponent):
 
     def compute_state(self):
-        self._output = not self.in_a.is_on
+        raise NotImplementedError("nocomp")
+
+    def get_state(self, port):
+        return getattr(self, port).get_state()
+
+    def build_circuit(self):
+        pass
 
 
-class LogicGate(SingleStateComponent):
-    """
-    Logic Gates have two inputs (in_a, in_b) connected to a single output
-    """
+class NAND(IntegratedComponent):
+    """NAND-Gate"""
 
     inputs = ElectricComponent.unpack_io('in_a', 'in_b')
+    outputs = ElectricComponent.unpack_io('out_a')
 
     def __init__(self, in_a: ElectricComponent = LooseWire(),
                  in_b: ElectricComponent = LooseWire()):
         self.in_a = in_a
         self.in_b = in_b
-        self.setup()
-
-
-class AND(LogicGate):
-    """AND-Gate"""
-
-    def build_circuit(self):
-        self.in_a.add_connection(self, 'in_a')
-        self.in_b.add_connection(self, 'in_b')
-
-    def compute_state(self):
-        self._output = self.in_a.is_on and self.in_b.is_on
-
-
-class OR(LogicGate):
-    """OR-Gate"""
-
-    def build_circuit(self):
-        self.in_a.add_connection(self, 'in_a')
-        self.in_b.add_connection(self, 'in_b')
-
-    def compute_state(self):
-        self._output = self.in_a.is_on or self.in_b.is_on
-
-
-class NAND(LogicGate):
-    """NAND-Gate"""
-
-    def build_circuit(self):
         self.AND1 = AND(self.in_a, self.in_b)
-        self.INV1 = INV(self.AND1)
-        self.INV1.add_connection(self, '_output')
+        self.out_a = INV(self.AND1)
 
-    def compute_state(self):
-        self._output = self.INV1.is_on
 
+
+a = Switch(True)
+b = Switch(False)
+nand1 = NAND(a, b)
+nand2 = NAND(nand1, b)
+nand3 = NAND(nand1, nand2)
+nand2.get_state()
+nand3.get_state()
+b.flip()
 
 class NOR(LogicGate):
     """NOR-Gate"""
 
     def build_circuit(self):
         self.OR1 = OR(self.in_a, self.in_b)
-        self.INV1 = INV(self.OR1)
-        self.INV1.add_connection(self, '_output')
-
-    def compute_state(self):
-        self._output = self.INV1.is_on
+        self._output = INV(self.OR1)
 
 
 class XOR(LogicGate):
@@ -84,8 +52,4 @@ class XOR(LogicGate):
     def build_circuit(self):
         self.OR1 = OR(self.in_a, self.in_b)
         self.NAND1 = NAND(self.in_a, self.in_b)
-        self.AND1 = AND(self.OR1, self.NAND1)
-        self.AND1.add_connection(self, '_output')
-
-    def compute_state(self):
-        self._output = self.AND1.is_on
+        self._output = AND(self.OR1, self.NAND1)
